14c4268bc52fa83616f611eb496e2a6a40305251
MadMks <mks59k@gmail.com>
2018-05-12 21:56:45 +0300

    Рабочая версия с секундной стрелкой.

---
+++ Task_1_Clock/FormClock.Designer.cs
@@ -0,0 +1,65 @@
+﻿namespace Task_1_Clock
+{
+    partial class FormClock
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(250, 250);
+            this.pictureBox.TabIndex = 0;
+            this.pictureBox.TabStop = false;
+            // 
+            // FormClock
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(250, 250);
+            this.Controls.Add(this.pictureBox);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "FormClock";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.FormClock_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox;
+    }
+}
+
---
+++ Task_1_Clock/FormClock.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Task_1_Clock
+{
+    public partial class FormClock : Form
+    {
+        private const int SECOND_PER_MINUTE = 60;
+
+        private Timer timer;
+        private Point centerPoint;
+        private Graphics graphics;
+        private Bitmap bmp;
+
+        public FormClock()
+        {
+            InitializeComponent();
+        }
+
+        private void FormClock_Load(object sender, EventArgs e)
+        {
+            //this.ChangeTheShapeToRound();
+
+            this.timer = new Timer();
+            this.timer.Interval = 1000;
+            this.timer.Tick += Timer_Tick;
+            this.timer.Start();
+
+            this.centerPoint = new Point(ClientSize.Width / 2, ClientSize.Height / 2);
+
+            this.bmp = new Bitmap(ClientSize.Width, ClientSize.Height);
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            //this.graphics = Graphics;
+            this.graphics = Graphics.FromImage(this.bmp);
+            graphics.Clear(Color.LightSalmon);
+
+            int ss = DateTime.Now.Second;
+            int mm = DateTime.Now.Minute;
+            int hh = DateTime.Now.Hour;
+
+            // Секундная стрелка
+            this.graphics.DrawLine(
+                new Pen(Brushes.Blue, 2),
+                this.centerPoint,
+                this.ComputeArrow(this.centerPoint, ss, 80));
+
+            
+
+            for (int i = 0; i < SECOND_PER_MINUTE; i++)
+            {
+                //this.graphics.DrawLine(
+                //    new Pen(Brushes.Black, 2),
+                //    this.ComputeArrow(this.centerPoint, i, 80),
+                //    this.ComputeArrow(this.centerPoint, i, 83));
+                this.graphics.DrawEllipse(new Pen(Brushes.Red, 2), new Rectangle(
+                    ComputeArrow(centerPoint, i, 80), new Size(4, 4)
+                    ));
+            }
+
+            // test
+            this.Text = $"{hh}:{mm}:{ss}";
+
+            this.pictureBox.Image = this.bmp;
+
+            this.graphics.Dispose();
+        }
+
+        private Point ComputeArrow(Point center, int seconds, int R)
+        {
+            int x = (int)(center.X + R * Math.Cos(
+                    -Math.PI / 2 + Math.PI * seconds / 30));
+            int y = (int)(center.Y + R * Math.Sin(
+                -Math.PI / 2 + Math.PI * seconds / 30));
+
+            return new Point(x, y);
+        }
+
+        private void ChangeTheShapeToRound()
+        {
+            this.FormBorderStyle = FormBorderStyle.None;
+
+            GraphicsPath formPath = new GraphicsPath();
+            formPath.AddEllipse(0, 0, this.Width, this.Height);
+            Region = new Region(formPath);
+        }
+    }
+}
